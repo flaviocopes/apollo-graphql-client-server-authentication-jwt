a692f5002695dc0710c329c96bb416e190c9b35c
Darek <dchrostowski@enterrasolutions.com>
2019-11-06 15:45:08 -0500

    Upgrading packages, setting client port

--- client/src/Form.js
+++ client/src/Form.js
@@ -4,51 +4,51 @@ import { navigate } from '@reach/router'
 const url = 'http://localhost:3000/login'
 
 const Form = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-  const submitForm = event => {
-    event.preventDefault()
+    const submitForm = event => {
+        event.preventDefault()
 
-    const options = {
-      method: 'post',
-      credentials: 'include',
-      headers: {
-        'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
-      },
-      body: `email=${email}&password=${password}`
+        const options = {
+            method: 'post',
+            credentials: 'include',
+            headers: {
+                'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
+            },
+            body: `email=${email}&password=${password}`
+        }
+
+        fetch(url, options)
+            .then(response => {
+                if (!response.ok) {
+                    if (response.status === 404) {
+                        alert('Email not found, please retry')
+                    }
+                    if (response.status === 401) {
+                        alert('Email and password do not match, please retry')
+                    }
+                }
+                return response
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    document.cookie = 'signedin=true'
+                    navigate('/private-area')
+                }
+            })
     }
 
-    fetch(url, options)
-    .then(response => {
-      if (!response.ok) {
-        if (response.status === 404) {
-          alert('Email not found, please retry')
-        }
-        if (response.status === 401) {
-          alert('Email and password do not match, please retry')
-        }
-      }
-      return response
-    })
-    .then(response => response.json())
-    .then(data => {
-      if (data.success) {
-        document.cookie = 'signedin=true'
-        navigate('/private-area')
-      }
-    })
-  }
-
-  return (
-    <div>
-      <form onSubmit={submitForm}>
-        <p>Email: <input type="text" onChange={event => setEmail(event.target.value)} /></p>
-        <p>Password: <input type="password" onChange={event => setPassword(event.target.value)} /></p>
-        <p><button type="submit">Login</button></p>
-      </form>
-    </div>
-  )
+    return (
+        <div>
+            <form onSubmit={submitForm}>
+                <p>Email: <input type="text" onChange={event => setEmail(event.target.value)} /></p>
+                <p>Password: <input type="password" onChange={event => setPassword(event.target.value)} /></p>
+                <p><button type="submit">Login</button></p>
+            </form>
+        </div>
+    )
 }
 
 export default Form
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloProvider } from 'react-apollo'
-import { setContext } from 'apollo-link-context'
 import { Router } from '@reach/router'
 import { navigate } from '@reach/router'
 import Cookies from 'js-cookie'
@@ -14,21 +13,20 @@ import PrivateArea from './PrivateArea'
 const httpLink = new HttpLink({ uri: 'http://localhost:3000/graphql', credentials: 'include' })
 
 const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache()
+    link: httpLink,
+    cache: new InMemoryCache()
 })
 
 if (Cookies.get('signedin')) {
-  navigate('/private-area')
+    navigate('/private-area')
 }
 
-
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <Router>
-      <Form path="/" />
-      <PrivateArea path="/private-area" />
-    </Router>
-  </ApolloProvider>,
-  document.getElementById('root')
+    <ApolloProvider client={client}>
+        <Router>
+            <Form path="/" />
+            <PrivateArea path="/private-area" />
+        </Router>
+    </ApolloProvider>,
+    document.getElementById('root')
 )
